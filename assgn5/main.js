bb2a4837b0e3f17f31c7b4fedd6fa665338f6169
Rituparna Roy <rituparnaroy@cmu.edu>
2019-10-23 23:25:53 -0400

    Update main.js

--- assgn5/main.js
+++ assgn5/main.js
@@ -1,10 +1,11 @@
 console.log('here');
-
+//declaring global variable
 var z = 0;
 
+//function to update page based on glaze selection
 function myFunction1() {
   var x = document.getElementById("mySelect1").value;
-  document.getElementById("demo1").innerHTML = "You selected: " + x;
+  document.getElementById("demo1").innerHTML = "You selected: " + x; //selection obtained
 
   if (x==="none")
   	document.getElementById("myImg").src = "walnut.jpg";
@@ -13,30 +14,35 @@ function myFunction1() {
   else if (x==="vanilla-milk")
   	document.getElementById("myImg").src = "walnut-vanilla-milk.jpg";
   else if (x==="double-chocolate")
-  	document.getElementById("myImg").src = "walnut-double-chocolate.jpg";
+  	document.getElementById("myImg").src = "walnut-double-chocolate.jpg"; //based on value of x, different images are updated
 }
 
+//function to update page based on quantity selection
 function myFunction2() {
   var y = document.getElementById("mySelect2").value;
-  document.getElementById("demo2").innerHTML = "You selected: " + y;
+  document.getElementById("demo2").innerHTML = "You selected: " + y; //selection obtained
   z = y;
 }
 
+//function to update page based on clicking add to cart button
 function myFunction3() {
   //document.getElementById("demo3").innerHTML = "Your selection is added to cart";
-  alert("Your selection is added to cart");
+  alert("Your selection is added to cart"); //updating the page with message
   console.log('1');
 }
+
+
 if(! count){
 	var count =0;
 	var number =0;
 }
 
+// function to update the cart with the number of items
 function incrementCount(){
 	count++;
-	number = number + parseInt(z);
+	number = number + parseInt(z); //convert z from string to integer 
 	//document.getElementById("demo3").innerHTML = "Number of items in your cart: " + count;
 	var w = document.getElementById("cart-number");
-	w.innerHTML = "(" + number + ")";
+	w.innerHTML = "(" + number + ")"; //paste actual item count
 	//document.getElementById("cart-number").innerHTML = "(" + number +")";
 }
