93739a02f06f95f8924ec201467bee3a75c79f32
Rituparna Roy <rituparnaroy@cmu.edu>
2019-11-03 14:46:42 -0500

    Update main.js

--- assgn6b/main.js
+++ assgn6b/main.js
@@ -1,7 +1,11 @@
+//testing console
 console.log('here');
 
+//declaring some variables used below
 var z = 0;
+var u= 0;
 
+//function to update user on glaze choices by calling it 'onchange'
 function myFunction1() {
   var x = document.getElementById("mySelect1").value;
   document.getElementById("demo1").innerHTML = "You selected: " + x;
@@ -16,22 +20,36 @@ function myFunction1() {
   	document.getElementById("myImg").src = "walnut-double-chocolate.jpg";
 }
 
+//function to update user on quantity choice by calling it 'onchange'
 function myFunction2() {
   var y = document.getElementById("mySelect2").value;
   document.getElementById("demo2").innerHTML = "You selected: " + y;
   z = y;
+  u = y*8;
+  //console.log(u);
 }
 
+//Add to cart, update storage
 function myFunction3() {
-  //document.getElementById("demo3").innerHTML = "Your selection is added to cart";
   alert("Your selection is added to cart");
-  console.log('1');
+  //console.log('1');
+  var myData = JSON.parse(localStorage.getItem("shopping-cart"))||[];
+  var newData =
+    [{'name': 'Walnut Cinnamon Roll','glaze': document.getElementById("mySelect1").value,
+      'count': document.getElementById("mySelect2").value,'price': u
+    }];
+  myData.push(newData);
+  localStorage.setItem("shopping-cart", JSON.stringify(myData));
+  var newData1 = JSON.parse(localStorage.getItem("shopping-cart"));
+  console.log(newData1);
+  //console.log(newData1[0][0].glaze);
 }
+
+//updating cart number details without storage
 if(! count){
 	var count =0;
 	var number =0;
 }
-
 function incrementCount(){
 	count++;
 	number = number + parseInt(z);
